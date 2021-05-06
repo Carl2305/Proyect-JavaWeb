f64f453a902464dadb9182a8c3b003293846ba4f
Carlos Mogollon <mogollon.23carlos05@gmail.com>
2021-05-06 11:07:22 -0500

    Version 1.0 conexion MySQL

--- ProyectGamesWeb/src/utils/Cnx_MySQL.java
+++ ProyectGamesWeb/src/utils/Cnx_MySQL.java
@@ -1,5 +1,33 @@
 package utils;
 
-public class Cnx_MySQL {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Cnx_MySQL {
+	public static Connection getConexion() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://localhost/ciberfarma?useSSL=false&useTimezone=true&serverTimezone=UTC";
+			String usr = "root";
+			String psw = "";
+			con = DriverManager.getConnection(url, usr, psw);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error >> Driver no Instalado!!" + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("Error >> de conexión con la BD" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error >> general : " + e.getMessage());
+		}
+		return con;
+	}
+
+	public static void closeConexion(Connection con) {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("Problemas al cerrar la conexion");
+		}
+	}
 }
